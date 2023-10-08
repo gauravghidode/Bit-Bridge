f4df239a25d845d58a90d444c913c8de37efd61e
Gaurav <gauravghidode@gmail.com>
2023-10-08 23:51:11 +0530

    Adding password validation on signup page

--- client/src/Pages/Auth/AboutAuth.jsx
+++ client/src/Pages/Auth/AboutAuth.jsx
@@ -6,7 +6,7 @@ const AboutAuth = () => {
         <h1>Join the Bit Bridge community</h1>
         <p>Ask a question and get your problems resolved.</p>
         <p>Answer a question and help others in their journey.</p>
-        <p>Save your favourite filters and topics,</p>
+        <p>Save your favourite filters and topics.</p>
         <p>Discuss about coding contests and interviews.</p>
 
     </div>
--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -1,11 +1,11 @@
-import React, {useState} from 'react'
-import {useDispatch} from 'react-redux'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { useNavigate } from "react-router-dom";
 
 import './Auth.css';
 import icon from '../../assets/logo.png'
 import AboutAuth from './AboutAuth';
-import {signUp, logIn} from '../../actions/auth'
+import { signUp, logIn } from '../../actions/auth'
 
 
 const Auth = () => {
@@ -19,69 +19,78 @@ const Auth = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleSwitch = ()=>{
+  function check(pass){
+    var flag1=0, flag2=0;
+    for(var i=0;i<pass.length;i++){
+      let ch=pass[i];
+      flag1=flag1|| (ch >= '0' && ch <= '9')
+      flag2=flag2||((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'));
+    }
+    return flag1&&flag2;
+  }
+
+  const handleSwitch = () => {
     setIsSignup(!isSignup)
   }
 
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if(!email || !password){
+    if (!email || !password) {
       alert("Enter Email and Password.");
     }
-    let pass=[...password]
-    if(isSignup){
-      if(password.length<8 || !pass.find((char)=>{
-        return (char>=48 && char<=57)||(char>=65 && char<=90)||(char>=97&&char<=112)
-      })){
+    let pass = [...password]
+    // console.log(pass);
+    if (isSignup) {
+      if (password.length < 8 || !check(pass)){
         alert("Password must contain atleast 8 characters, including atleast 1 number and 1 letter.");
       }
-      else if(!name){
+      else if (!name) {
         alert("Enter your name to continue");
-      }else
-        dispatch(signUp({name, email, password}, navigate))
+      } else
+        dispatch(signUp({ name, email, password }, navigate))
     }
-    else{
-      try{
-        dispatch(logIn({email, password}, navigate))
-      }catch(e){
+    else {
+      try {
+        dispatch(logIn({ email, password }, navigate))
+      } catch (e) {
         alert("Invalid credentials");
       }
-      
+
     }
   }
 
   return (
     <section className='auth-section'>
-      {isSignup && <AboutAuth/>}
+      {isSignup && <AboutAuth />}
       <div className='auth-container-2 '>
-        {!isSignup && <img src={icon} alt='Bit Bridge' className='login-logo' /> }
+        {!isSignup && <img src={icon} alt='Bit Bridge' className='login-logo' />}
         <form action="" onSubmit={handleSubmit}>
           {
             isSignup && (
               <label htmlFor='name'>
                 <h4>Display Name</h4>
-                <input type="text" name="name" id="name" placeholder='Enter your name' onChange={(e)=>{setName(e.target.value)}} />
+                <input type="text" name="name" id="name" placeholder='Enter your name' onChange={(e) => { setName(e.target.value) }} />
               </label>
             )
           }
           <label htmlFor="email">
             <h4>Email</h4>
-            <input type="email" name='email' id='email' placeholder='Enter your email' onChange={(e)=>{setEmail(e.target.value)}}  />
+            <input type="email" name='email' id='email' placeholder='Enter your email' onChange={(e) => { setEmail(e.target.value) }} />
           </label>
           <label htmlFor="password">
             <div>
               <h4>Password</h4>
             </div>
-            <input type="password" name='password' placeholder ='Enter password' id='password' onChange={(e)=>{setPassword(e.target.value)}}  />
-            {isSignup && <p style={{color: "#666767", fontSize: "13px"}}>Password must contain atleast 8 characters,<br/>including atleast 1 number and 1 letter.</p>}
+            <input type="password" name='password' placeholder='Enter password' id='password' onChange={(e) => { setPassword(e.target.value) }} />
+            {isSignup && <p style={{ color: "#666767", fontSize: "13px" }}>Password must contain atleast 8 characters,<br />including atleast 1 number and 1 letter.</p>}
           </label>
-              {!isSignup && <p style={{color: '#007ac6'}}>Forgot password?</p>}
-          
-          <button type='submit' className='auth-btn border-gradient border-gradient-purple grad-btn'>{isSignup? 'Sign up': 'Login'}</button>
+          {!isSignup && <p style={{ color: '#007ac6' }}>Forgot password?</p>}
+
+          <button type='submit' className='auth-btn border-gradient border-gradient-purple grad-btn'>{isSignup ? 'Sign up' : 'Login'}</button>
         </form>
         <p>
-          {isSignup? "Already have an account?" : "Don't have an account?"}
-          <button type='button' className='handle-switch-btn ' onClick={handleSwitch}>{!isSignup? 'Sign up': 'Login'}</button>
+          {isSignup ? "Already have an account?" : "Don't have an account?"}
+          <button type='button' className='handle-switch-btn ' onClick={handleSwitch}>{!isSignup ? 'Sign up' : 'Login'}</button>
         </p>
       </div>
     </section>
