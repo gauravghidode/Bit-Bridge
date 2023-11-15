3044874732e210cb162bea61cb4679336fa70881
Alok Verma <alokverma73762@gmail.com>
2023-11-15 12:46:09 +0530

    Adding result section to the user database

--- server/models/auth.js
+++ server/models/auth.js
@@ -6,7 +6,18 @@ const userSchema = mongoose.Schema({
     password: {type: String, required: true},
     about: {type: String},
     tags: {type: [String]},
-    joinedOn: {type: Date, default: Date.now}
+    joinedOn: {type: Date, default: Date.now},
+    role: {type: String, enum: ["student", "admin", "instructor"], default: 'student'},
+    result: [
+        {
+            quizId:{
+                type: mongoose.Schema.Types.ObjectId,
+            },
+            marks: {
+                type: Number
+            }
+        }
+    ]
 })
 
 export default mongoose.model("User", userSchema);
