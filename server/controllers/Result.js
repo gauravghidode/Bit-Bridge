2be94127a745eccdf8be166167e32052bb43d526
Alok Verma <alokverma73762@gmail.com>
2023-11-20 00:54:29 +0530

    "Now Instructor can see all student report"

--- server/controllers/Result.js
+++ server/controllers/Result.js
@@ -24,21 +24,37 @@ export const getMyResult = async (req, res)=>{
     }
 }
 
-// export const QuizResult = async (req, res)=>{
-//     try {
-//         const {quizid, userid} = req.params;
-//         const user = await User.findById(userid);
-//         if(user.role !== "admin" || user.role !== "instructor"){
-//             return res.status(403).json({
-//                 success: false,
-//                 message: "Your are not allowed to see this section"
-//             })
-//         }
-//         const quiz = await Quiz.findById({})
-//     } catch (error) {
-        
-//     }
-// }
+export const QuizResult = async (req, res)=>{
+    try {
+        const {quizid, userid} = req.params;
+        const user = await User.findById(userid);
+        if(user.role !== "admin" && user.role !== "instructor"){
+            return res.status(403).json({
+                success: false,
+                message: "Your are not allowed to see this section",
+                user:user.role
+            })
+        }
+        console.log("reached");
+        const quiz = await Quiz.findById(quizid).populate({
+            path:"user",
+            populate:{
+                path: "userName",
+                select:{name: true, email: true,}
+            }
+        })
+        console.log("reached also");
+        return res.status(200).json({
+            quiz,
+            message: "fetched successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message: "Something went wrong"
+        })
+    }
+}
 export const Quizzes = async (req, res)=>{
     try {
         const {userid} = req.params;
--- server/routes/Result.js
+++ server/routes/Result.js
@@ -1,11 +1,11 @@
 import express from 'express'
 
-import { Quizzes, getMyResult } from "../controllers/Result.js";
+import { QuizResult, Quizzes, getMyResult } from "../controllers/Result.js";
 
 const router = express.Router();
 
 router.get('/getMyResult/:userid', getMyResult);
 router.get('/:userid', Quizzes);
-// router.get('/getSubjectQuestions/:subjectId', getSubjectQuestion);
+router.get('/quizResult/:userid/:quizid', QuizResult);
 
 export default router;
